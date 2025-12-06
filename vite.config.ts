2f5cbeea4f131c9ae5271fc24715768c392cce90
Charlie203 <122697967+Charlie203@users.noreply.github.com>
2025-12-06 17:43:09 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 📌 Added this part to remove the 500kb warning
+  build: {
+    chunkSizeWarningLimit: 1000, // you can adjust this number if needed
+  },
 })
